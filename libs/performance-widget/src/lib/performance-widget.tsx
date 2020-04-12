5f4e6d34d24e5e942a01bf7402bd5174519d217b
Tracy <trlee@vestmark.com>
2020-04-12 11:11:40 -0500

    include recharts as dep

--- libs/performance-widget/src/lib/performance-widget.tsx
+++ libs/performance-widget/src/lib/performance-widget.tsx
@@ -13,6 +13,15 @@ import {
   Button,
   ButtonGroup
 } from '@material-ui/core';
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  ResponsiveContainer
+} from 'recharts';
 
 export interface PerformanceWidgetProps {
   apiKey: string;
@@ -29,8 +38,8 @@ const useStyles = makeStyles(theme => ({
     }
   },
   formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120
+    minWidth: 120,
+    marginBottom: theme.spacing(0.75)
   },
   selectEmpty: {
     marginTop: theme.spacing(2)
@@ -42,50 +51,107 @@ const useStyles = makeStyles(theme => ({
 
 export const PerformanceWidget = (props: PerformanceWidgetProps) => {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [accountName, setAccountName] = React.useState('');
   const handleChange = event => {
-    setAge(event.target.value);
+    setAccountName(event.target.value);
   };
+  const chartData = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100
+    }
+  ];
 
   return (
     <Card>
       <CardContent>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Typography className={classes.title} gutterBottom>
-              <Grid container justify="space-between" spacing={3}>
-                <Grid item>
-                  Title: {props.title}, Api Key: {props.apiKey}
-                </Grid>
-                <Grid item>
-                  <ButtonGroup
-                    variant="text"
-                    color="primary"
-                    size="small"
-                    aria-label="text primary button group"
-                  >
-                    <Button>Week</Button>
-                    <Button>Month</Button>
-                    <Button>Year</Button>
-                  </ButtonGroup>
-                </Grid>
+            <Grid container justify="space-between" spacing={3}>
+              <Grid item alignContent="center">
+                <Typography className={classes.title}>
+                  {props.title} (api key: {props.apiKey})
+                </Typography>
               </Grid>
-            </Typography>
+              <Grid item alignContent="center">
+                <ButtonGroup
+                  variant="text"
+                  color="primary"
+                  size="small"
+                  aria-label="text primary button group"
+                >
+                  <Button>Week</Button>
+                  <Button>Month</Button>
+                  <Button>Year</Button>
+                </ButtonGroup>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item xs={6}>
             <FormControl className={classes.formControl}>
               <InputLabel id="demo-simple-select-label">Accounts</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                onChange={handleChange}
-              >
+              <Select value={accountName} onChange={handleChange}>
                 <MenuItem value={10}>Ten</MenuItem>
                 <MenuItem value={20}>Twenty</MenuItem>
                 <MenuItem value={30}>Thirty</MenuItem>
               </Select>
             </FormControl>
+          </Grid>
+          <Grid xs={12}>
+            <div style={{ width: '100%', height: 300 }}>
+              <ResponsiveContainer>
+                <LineChart
+                  width={600}
+                  height={300}
+                  data={chartData}
+                  margin={{ top: 25, right: 20, bottom: 5, left: 0 }}
+                >
+                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
           </Grid>
         </Grid>
       </CardContent>
